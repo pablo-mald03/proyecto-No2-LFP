9414686fb2026f2fc650a7b3b0bb48d652ce58fa
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-10-11 17:58:13 -0600

    Se ha inicializado el repositorio del ProyectoNo2LFP

---
+++ src/main/java/com/pablocompany/proyectono2lfp/ProyectoNo2LFP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pablocompany.proyectono2lfp;
+
+/**
+ *
+ * @author pablo
+ */
+public class ProyectoNo2LFP {
+
+    //Se encarga de inicializar la aplicacion
+    public static void main(String[] args) {
+     
+    }
+}
